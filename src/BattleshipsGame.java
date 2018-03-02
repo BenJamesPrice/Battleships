a4451df8ab1aab43d52325dafd5b897cb877ef6f
U-ben-PC\ben <benjprice99@gmail.com>
2018-03-02 19:43:59 +0000

    Completed setupgame, startgame, getUserInput, checkUserGuess, finishGame, main methods

--- src/BattleshipsGame.java
+++ src/BattleshipsGame.java
@@ -1,28 +1,85 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class BattleshipsGame {
 	
+	private ArrayList<Ship> ships;
+	private int numOfGuesses;
+	private Grid grid;
+	
 	private void setupGame() {
+		numOfGuesses = 0;
 		
-	}
-	
-	private void startgame() {
+		//Create grid
+		this.grid = new Grid();
 		
+		//Create 3 ships and add to ships ArrayList
+		ships = new ArrayList<Ship>();
+		Ship submarine = new Ship();
+		submarine.setName("Submarine");
+		submarine.setLocationCells(grid.getNewLocationCells());
+		Ship destroyer = new Ship();
+		destroyer.setName("Destroyer");
+		destroyer.setLocationCells(grid.getNewLocationCells());
+		Ship carrier = new Ship();
+		carrier.setName("Aircraft Carrier");
+		carrier.setLocationCells(grid.getNewLocationCells());
+		ships.add(submarine);
+		ships.add(destroyer);
+		ships.add(carrier);
 	}
 	
-	private String getUserInput() {
-		return null;
+	//Continues playing the game while there are ships alive
+	private void startGame() {
+		while (!ships.isEmpty()) {
+			String guess = getUserInput("Enter guess: ");
+			checkUserGuess(guess);
+		}
+		finishGame();
 	}
 	
-	private ArrayList<String> getNewLocation() {
-		return null;
+	//Get the user's coordinate input as a string
+	private String getUserInput(String prompt) {
+		String inputLine = null;
+		System.out.print(prompt);
+		try {
+			BufferedReader is = new BufferedReader(
+			new InputStreamReader(System.in));
+			inputLine = is.readLine();
+			if (inputLine. length () == 0) return null;
+		} catch (IOException e) {
+			System.out.println("IOException: " + e);
+		}
+		return inputLine;
 	}
 	
+	//Compares the user's guess to each ship's locations
+	private void checkUserGuess(String guess) {
+		numOfGuesses++;
+		String result = "miss";
+		for (Ship s : ships) {
+			result = s.checkHit(guess);
+			if (result == "hit") {
+				break;
+			} else if (result == "kill") {
+				ships.remove(s);
+				break;
+			}
+		}
+		System.out.println(result);
+	}
+	
+	//Prints out messages once game is over
 	private void finishGame() {
-		
+		System.out.println("Well done!");
+		System.out.println("You took " + numOfGuesses + " guesses.");
 	}
-	
-	public static void main(String[] args) {
 		
+	public static void main(String[] args) {
+		BattleshipsGame game = new BattleshipsGame();
+		game.setupGame();
+		game.startGame();
 	}
 }

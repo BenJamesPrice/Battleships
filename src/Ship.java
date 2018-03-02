90ab02c57bd107a730ca4cb012944c231a3fa77c
U-ben-PC\ben <benjprice99@gmail.com>
2018-03-02 19:34:08 +0000

    Completed setName, getName, setLocationCells, checkHit methods

--- src/Ship.java
+++ src/Ship.java
@@ -4,19 +4,29 @@ public class Ship {
 	private String name;
 	private ArrayList<String> locationCells;
 	
-	public void setName() {
-		
+	public void setName(String n) {
+		this.name = n;
 	}
 	
 	public String getName() {
-		return null;
+		return this.name;
 	}
 	
-	public void setLocationCells() {
-		
+	public void setLocationCells(ArrayList<String> locs) {
+		this.locationCells = locs;
 	}
 	
-	public void checkHit() {
-		
+	public String checkHit(String guess) {
+		String result = "miss";
+		int index = locationCells.indexOf(guess);
+		if (index >= 0) {
+			locationCells.remove(index);
+			if (locationCells.isEmpty()) {
+				result = "kill";
+			} else {
+				result = "hit";
+			}
+		}
+		return result;
 	}
 }

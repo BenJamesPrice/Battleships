00c433c1b0a8111c17904ee1bb1cc3c07d786e6a
U-ben-PC\ben <benjprice99@gmail.com>
2018-03-02 18:10:40 +0000

    Created source files with empty methods

---
+++ src/BattleshipsGame.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class BattleshipsGame {
+	
+	private void setupGame() {
+		
+	}
+	
+	private void startgame() {
+		
+	}
+	
+	private String getUserInput() {
+		return null;
+	}
+	
+	private ArrayList<String> getNewLocation() {
+		return null;
+	}
+	
+	private void finishGame() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/Ship.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Ship {
+	private String name;
+	private ArrayList<String> locationCells;
+	
+	public void setName() {
+		
+	}
+	
+	public String getName() {
+		return null;
+	}
+	
+	public void setLocationCells() {
+		
+	}
+	
+	public void checkHit() {
+		
+	}
+}

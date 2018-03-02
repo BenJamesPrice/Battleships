dc220aa030f1c16a4975e589c84328dd82784fe3
U-ben-PC\ben <benjprice99@gmail.com>
2018-03-02 18:29:20 +0000

    Fixed Constructor bug and added display grid method

--- src/Grid.java
+++ src/Grid.java
@@ -8,12 +8,21 @@ public class Grid {
 		this.grid = new String[7][7];
 		for (int i = 0; i<grid.length; i++) {
 			for (int j = 0; j<grid[i].length; j++) {
-				grid[i][i] = "";
+				grid[i][j] = "?";
 			}
 		}
 	}
 
 	public ArrayList<String> getNewLocationCells() {
 		return null;
+	}
+	
+	public void displayGrid() {
+		for (int i = 0; i<grid.length; i++) {
+			for (int j = 0; j<grid[i].length; j++) {
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }

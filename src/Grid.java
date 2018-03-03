9194b44d94f4fabc21c7421eb6aeebb0ee06ae3c
U-ben-PC\ben <benjprice99@gmail.com>
2018-03-03 11:05:56 +0000

    setupGrid method fills grid with '?'. Added method to display grid

--- src/Grid.java
+++ src/Grid.java
@@ -18,8 +18,17 @@ public class Grid {
 		grid = new String[7][7];
 		for (int i = 0; i<grid.length; i++) {
 			for (int j = 0; j<grid[i].length; j++) {
-				grid[i][j] = "";
+				grid[i][j] = "?";
 			}
+		}
+	}
+	
+	public void displayGrid() {
+		for (int i = 0; i<grid.length; i++) {
+			for (int j = 0; j<grid[i].length; j++) {
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
 		}
 	}
 	

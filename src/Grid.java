7776827e56b7c9a56eaf52e77926f55fe886fbb8
U-ben-PC\ben <benjprice99@gmail.com>
2018-03-03 10:56:34 +0000

    Updated updateGrid method and created setupGrid method

--- src/Grid.java
+++ src/Grid.java
@@ -2,25 +2,43 @@ import java.util.ArrayList;
 
 public class Grid {
 	
-	private ArrayList<String> columns;
+	private String[][] grid;
+	private ArrayList<String> rows;
 	
 	public Grid() {
-		columns = new ArrayList<String>();
+		rows = new ArrayList<String>();
 		String alphabet = "ABCDEFG";
 		for (int i = 0; i < alphabet.length(); i++) {
-			columns.add(String.valueOf(alphabet.charAt(i)));
+			rows.add(String.valueOf(alphabet.charAt(i)));
+		}
+		setupGrid();
+	}
+	
+	private void setupGrid() {
+		grid = new String[7][7];
+		for (int i = 0; i<grid.length; i++) {
+			for (int j = 0; j<grid[i].length; j++) {
+				grid[i][j] = "";
+			}
 		}
 	}
 	
 	public void updateGrid(String coords, String result) {
-		
+		char rowLetter = coords.charAt(0);
+		int rowIndex = rowLetter - 'A';
+		int colIndex = Integer.parseInt(String.valueOf(coords.charAt(1)));
+		String r = "m";
+		if (result == "hit" || result == "kill") {
+			r = "h";
+		}
+		grid[rowIndex][colIndex] = r;
 	}
 	
 	public ArrayList<String> getNewLocationCells() {
 		ArrayList<String> locations = new ArrayList<String>();
-		int index = (int) (Math.random() * columns.size());
-		String letter = columns.get(index);
-		columns.remove(index);
+		int index = (int) (Math.random() * rows.size());
+		String letter = rows.get(index);
+		rows.remove(index);
 		int startingNumber = (int) (Math.random() * 5);
 		
 		for (int i = 0; i < 3; i++) {
